bc9b178701b2765cb11a8cf414b6c56c29dcfc7d
Your Name <your.email@example.com>
2025-06-17 17:17:29 +0530

    update code

---
+++ Frontend/.angular/cache/19.2.10/hrm/vite/deps/chunk-L3EGP364.js
@@ -0,0 +1,47 @@
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-UUF4KKBH.js";
+import {
+  MatCommonModule,
+  ObserversModule
+} from "./chunk-IPP63HGB.js";
+import {
+  NgModule,
+  setClassMetadata,
+  ɵɵdefineInjector,
+  ɵɵdefineNgModule
+} from "./chunk-IYMJOHNR.js";
+
+// node_modules/@angular/material/fesm2022/module-DAp_YJSv.mjs
+var MatFormFieldModule = class _MatFormFieldModule {
+  static ɵfac = function MatFormFieldModule_Factory(__ngFactoryType__) {
+    return new (__ngFactoryType__ || _MatFormFieldModule)();
+  };
+  static ɵmod = ɵɵdefineNgModule({
+    type: _MatFormFieldModule,
+    imports: [MatCommonModule, ObserversModule, MatFormField, MatLabel, MatError, MatHint, MatPrefix, MatSuffix],
+    exports: [MatFormField, MatLabel, MatHint, MatError, MatPrefix, MatSuffix, MatCommonModule]
+  });
+  static ɵinj = ɵɵdefineInjector({
+    imports: [MatCommonModule, ObserversModule, MatCommonModule]
+  });
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(MatFormFieldModule, [{
+    type: NgModule,
+    args: [{
+      imports: [MatCommonModule, ObserversModule, MatFormField, MatLabel, MatError, MatHint, MatPrefix, MatSuffix],
+      exports: [MatFormField, MatLabel, MatHint, MatError, MatPrefix, MatSuffix, MatCommonModule]
+    }]
+  }], null, null);
+})();
+
+export {
+  MatFormFieldModule
+};
+//# sourceMappingURL=chunk-L3EGP364.js.map
